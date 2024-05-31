83bcce5dfe85b450d1ed0fdbdd1d00064536f908
igbanibo-Dengi <igbanibodengi@gmail.com>
2024-05-31 06:28:00 +0100

    "Refactor loading animation and add loading state to Home page"

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,9 +1,22 @@
 export default function Loading() {
     return (
-        <div className="loader">
-            <span className="bar"></span>
-            <span className="bar"></span>
-            <span className="bar"></span>
+        <div className="text-center flex flex-col h-screen w-full items-center justify-center bg-black">
+            <div className="pyramid-loader">
+                <div className="wrapper">
+                    <span className="side side1"></span>
+                    <span className="side side2"></span>
+                    <span className="side side3"></span>
+                    <span className="side side4"></span>
+                    <span className="shadow"></span>
+                </div>
+
+            </div>
+            {/* <p className="capitalize text-white font-semibold text-2xl text-nowrap text-center absolute top-[70%] w-full">Partake in the majestic grandeur that follows</p> */}
+
         </div>
+
+
     )
 }
+
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,54 @@
+'use client'
+
 import Countdown from "@/components/Countdown";
 import ImageContainer from "@/components/ImageContainer";
 import { VelocityScroll } from "@/components/VelocityScroll";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 import { BiLinkExternal } from "react-icons/bi";
 import { FaInstagram } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
+import Loading from "./loading";
 
 export default function Home() {
-  return (
-    <main className="flex flex-col h-screen items-end justify-center overflow-hidden relative">
-      <div className="bg-[url('/image.png')] bg-cover absolute xl:hidden h-screen w-full"></div>
+  const [loading, setLoading] = useState(true);
 
-      <div className="w-full  absolute left-0 text-white md:text-black">
-        <VelocityScroll />
-        <div className="flex items-center justify-center xl:w-1/2">
-          <Countdown timeTillDate="06 07 2024, 6:00 am" timeFormat="MM DD YYYY, h:mm a" />
+  useEffect(() => {
+    // Simulate loading delay, replace with actual loading logic
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 500); // Adjust the timeout as needed
+
+    return () => clearTimeout(timer);
+  }, []);
+
+
+  return (
+    <>
+      {loading && <Loading />}
+      {!loading && <main className="flex flex-col h-screen items-end justify-center overflow-hidden relative">
+        <div className="bg-[url('/image.png')] bg-cover absolute xl:hidden h-screen w-full"></div>
+
+        <div className="w-full  absolute left-0 text-white md:text-black">
+          <VelocityScroll />
+          <div className="flex items-center justify-center xl:w-1/2">
+            <Countdown timeTillDate="06 07 2024, 6:00 am" timeFormat="MM DD YYYY, h:mm a" />
+          </div>
+          <VelocityScroll />
         </div>
-        <VelocityScroll />
-      </div>
-      <ImageContainer />
-      <div className="absolute bottom-2 md:w-1/2 flex gap-4 items-center justify-center left-5 z-50 text-white xl:text-black text-[24px] md:text-[48px] xl:text-[24px]">
-        <Link href="https://falzthebahdguy.com/" target="blank">
-          <BiLinkExternal />
-        </Link>
-        <Link href="https://www.instagram.com/falzthebahdguy/" target="blank">
-          <FaInstagram />
-        </Link>
-        <Link href="https://x.com/falzthebahdguy" target="blank">
-          <FaXTwitter />
-        </Link>
-      </div>
-    </main>
+        <ImageContainer />
+        <div className="absolute bottom-2 md:w-1/2 flex gap-4 items-center justify-center left-5 z-50 text-white xl:text-black text-[24px] md:text-[48px] xl:text-[24px]">
+          <Link href="https://falzthebahdguy.com/" target="blank">
+            <BiLinkExternal />
+          </Link>
+          <Link href="https://www.instagram.com/falzthebahdguy/" target="blank">
+            <FaInstagram />
+          </Link>
+          <Link href="https://x.com/falzthebahdguy" target="blank">
+            <FaXTwitter />
+          </Link>
+        </div>
+      </main>}
+    </>
   );
 }
